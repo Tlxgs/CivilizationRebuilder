0aa26829eaeb8433cddf2f109c8bcf273ceb6d29
Tlxgs <txc18151124842@163.com>
2026-05-02 23:59:25 +0800

    Add files via upload

--- utils.js
+++ utils.js
@@ -5,7 +5,7 @@ function formatNumber(n) {
     if (n < 1e3) return n.toFixed(2);
     if (n < 1e6) return (n/1e3).toFixed(2) + "K";
     if (n < 1e9) return (n/1e6).toFixed(2) + "M";
-    if (n < 1e12) return (n/1e9).toFixed(2) + "B";
+    if (n < 1e12) return (n/1e9).toFixed(2) + "G";
     return (n/1e12).toFixed(2) + "T";
 }
 
